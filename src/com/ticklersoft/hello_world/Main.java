54d54184fc4c764655743eb74c4e0e6a521d776f
Josef Bodnar <josef.bodnar@post.cz>
2020-04-04 14:06:25 +0200

    added comments - what program should do

--- src/com/ticklersoft/hello_world/Main.java
+++ src/com/ticklersoft/hello_world/Main.java
@@ -3,6 +3,16 @@ package com.ticklersoft.hello_world;
 public class Main {
 
     public static void main(String[] args) {
+        //aplikace na hádání čísel od 1 do 10
+
+        //po spuštění vyzve uživatele k zadání prvního čísla
+
+        // po zadání odpoví [samá voda, přihořívá, hoří]
+
+        // a vyzve hráče k zadání dalšího čísla a celý proces se opakuje, dokud hráč číslo neuhodne
+
+        // na konci vypíše počet pokusů uživatele
+
         int i = 0;
         i++;
         System.out.println("Hello World! " + i);

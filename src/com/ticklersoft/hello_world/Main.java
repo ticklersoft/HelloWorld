66547c16b725c60745534b7d2bbdf4c12dbb1942
Josef Bodnar <josef.bodnar@post.cz>
2020-04-04 13:58:00 +0200

    added hello world code

--- src/com/ticklersoft/hello_world/Main.java
+++ src/com/ticklersoft/hello_world/Main.java
@@ -3,6 +3,8 @@ package com.ticklersoft.hello_world;
 public class Main {
 
     public static void main(String[] args) {
-
+        int i = 0;
+        i++;
+        System.out.println("Hello World! " + i);
     }
 }

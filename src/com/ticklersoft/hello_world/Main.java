331e2e8cf7e320fc142bdef482f4f83e975b00d0
Michal1J <jun.misa@seznam.cz>
2020-04-17 21:33:47 +0200

    Pridana logika hadani

--- src/com/ticklersoft/hello_world/Main.java
+++ src/com/ticklersoft/hello_world/Main.java
@@ -4,17 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         //aplikace na hádání čísel od 1 do 10
+        System.out.println("Hádej číslo od 1 do 10");
 
         //po spuštění vyzve uživatele k zadání prvního čísla
-
+        int tipovaneCislo = 4;
         // po zadání odpoví [samá voda, přihořívá, hoří]
+        int cisloKUhadnuti = (int) (Math.random()*10);
+        System.out.println(cisloKUhadnuti);
+        if(tipovaneCislo == cisloKUhadnuti)   {
+            System.out.println("Hoří");
+            return;
+        }
+        int tolerancePrihorivani = 2;
+        if(Math.abs(tipovaneCislo - cisloKUhadnuti) <= tolerancePrihorivani)   {
+            System.out.println("Prihoriva");
+            return;
+        }
+        System.out.println("Sama voda");
 
         // a vyzve hráče k zadání dalšího čísla a celý proces se opakuje, dokud hráč číslo neuhodne
 
         // na konci vypíše počet pokusů uživatele
 
-        int i = 0;
-        i++;
-        System.out.println("Hello World! " + i);
+
+
     }
 }
+

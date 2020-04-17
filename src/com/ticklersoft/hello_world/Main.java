d9e5aaea0d1c067a46c7c437ea53f86249ea41c0
Michal1J <jun.misa@seznam.cz>
2020-04-17 22:05:09 +0200

    Priprava pro cyklus

--- src/com/ticklersoft/hello_world/Main.java
+++ src/com/ticklersoft/hello_world/Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         //aplikace na hádání čísel od 1 do 10
         System.out.println("Hádej číslo od 1 do 10");
+        int cisloKUhadnuti = (int) (Math.random()*10);
 
         //po spuštění vyzve uživatele k zadání prvního čísla
         Scanner sc = new Scanner(System.in);
-        int tipovaneCislo = sc.nextInt();
 
+        int tipovaneCislo = sc.nextInt();
         // po zadání odpoví [samá voda, přihořívá, hoří]
-        int cisloKUhadnuti = (int) (Math.random()*10);
         System.out.println(cisloKUhadnuti);
         if(tipovaneCislo == cisloKUhadnuti)   {
             System.out.println("Hoří");
@@ -22,10 +22,9 @@ public class Main {
         int tolerancePrihorivani = 2;
         if(Math.abs(tipovaneCislo - cisloKUhadnuti) <= tolerancePrihorivani)   {
             System.out.println("Prihoriva");
-            return;
+        } else {
+            System.out.println("Sama voda");
         }
-        System.out.println("Sama voda");
-
         // a vyzve hráče k zadání dalšího čísla a celý proces se opakuje, dokud hráč číslo neuhodne
 
         // na konci vypíše počet pokusů uživatele

16c47300ed04d322cff05074ce895162b938640d
Josef Bodnar <josef.bodnar@post.cz>
2020-05-15 22:39:54 +0200

    added comments

--- src/com/ticklersoft/hello_world/TextovyPokec.java
+++ src/com/ticklersoft/hello_world/TextovyPokec.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 
 public class TextovyPokec {
     public static void main(String[] args) {
+        //TODO dodelat do cyklu
         String zadanyText = "ahoj";
 //        String zadanyText = zadejTextUzivatele();
 
+        //TODO dodelat nejaka sledovana slova a reakce na ne
         if (zadanyText.contains("blaba")){
             System.out.println("odpoved");
         }
+        //if (zadanyText.contains("blaba") ...
+        //if (zadanyText.contains("blaba") ...
     }
 
     private static String zadejTextUzivatele() {

4f8f16aa0cb65994561d55be91737eba1802405d
Michal1J <jun.misa@seznam.cz>
2020-05-22 18:37:20 +0200

    pokus o přidání odpovědí

--- src/com/ticklersoft/hello_world/TextovyPokec.java
+++ src/com/ticklersoft/hello_world/TextovyPokec.java
@@ -20,5 +20,18 @@ public class TextovyPokec {
         Scanner sc = new Scanner(System.in);
         return sc.next();
     }
+         if (zadanyText1.contains("ahoj")){
+        System.out.println("ahoj");
+        else system.out.println("promiň zkus to říct nějak jinak") ;
+         }
 
-}
+         if (zadanyText2.contains("jak se máš?")){
+        System.out.println("dobře a ty?");
+        return sc.next();
+
+            if (zadanyText3.contains("spatně")){
+            System.out.println("co se stalo?");
+            else system.out.println("promiň zkus to říct nějak jinak") ;
+
+
+        }

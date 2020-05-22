e5e805c663b9e5a93299de9742d27b56dce832c1
Josef Bodnar <josef.bodnar@post.cz>
2020-05-22 21:40:02 +0200

    created mini conversation

--- src/com/ticklersoft/hello_world/TextovyPokecObjektove.java
+++ src/com/ticklersoft/hello_world/TextovyPokecObjektove.java
@@ -101,31 +101,4 @@ public class TextovyPokecObjektove {
             return sc.nextLine();
         }
     }
-
-
-//
-//            System.out.println("Vitej v kecacim programu!");
-//        System.out.println("Zkus na začátek hezky pozdravit... :-)");
-//
-//    //TODO dodelat do cyklu
-//
-//    String zadanyText = zadejTextUzivatele();
-//
-//        if (zadanyText.contains("ahoj")) {
-//        System.out.println("ahoj");
-//    } else {
-//        System.out.println("promiň zkus to říct nějak jinak");
-//    }
-//
-//        if (zadanyText.contains("jak se máš?")) {
-//        System.out.println("dobře a ty?");
-//    } else {
-//        System.out.println("promiň zkus to říct nějak jinak");
-//    }
-//
-//        if (zadanyText.contains("spatně")) {
-//        System.out.println("co se stalo?");
-//    } else {
-//        System.out.println("promiň zkus to říct nějak jinak");
-//    }
 }
